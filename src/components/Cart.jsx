a5903d396211d8d616dc9facfc833e387201a3c7
Victor Reyes <vicalregu@gmail.com>
2023-04-19 03:39:15 -0600

    Responsive fix #2

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -56,19 +56,19 @@ const Cart = () => {
         >
           <CartCount totalQTY={totalQTY} onCartToggle={onCartToggle} onClearCartItems={onClearCartItems} />
           {cartItems?.length === 0 ? <CartEmpty onCartToggle={onCartToggle} /> : <div>
-            <div className="flex items-start justify-start flex-col gap-y-7 lg:gap-y-5 overflow-y-scroll h-[75vh] scroll-smooth scroll-hidden py-5">
+            <div className="flex items-start justify-start flex-col gap-y-7 lg:gap-y-5 overflow-y-scroll h-[80vh] xsm:h-[70vh] scroll-smooth scroll-hidden py-4 pb-6">
               {cartItems?.map((item, i) => (
                 <CartItem key={i} item={item} />
               ))}
             </div>
 
-            <div className="fixed bottom-0 bg-white w-full px-5 py-2 grid items-center">
-              <div className="flex items-center justify-between">
-                <h1 className="text-base font-semibold uppercase">SubTotal</h1>
+            <div className="fixed bottom-0 bg-white w-full px-5 py-2 xsm:mb-14 grid items-center">
+              <div className="flex items-center justify-between mb-1">
+                <h1 className="text-base font-semibold uppercase">Subtotal</h1>
                 <h1 className="text-sm rounded bg-theme-cart text-slate-100 px-1 py-0.5">${totalAmount}</h1>
               </div>
               <div className="grid items-center gap-2">
-                <p className="text-sm sm:text-xs font-medium text-center">Taxes and Shipping Will Calculate At Shipping</p>
+                <p className="text-sm sm:text-xs font-medium text-center">Taxes and shipping will calculate at shipping</p>
                 <button type="button" className="button-theme bg-theme-cart text-white">Check Out</button>
               </div>
             </div>
